dc6a78023e762f142b37ccf066608833052b695a
Brian Dill <brian.dill@randasolutions.com>
2021-03-16 14:21:09 -0500

    Create adm_ReservedKeywords_Detect.sql

---
+++ adm_ReservedKeywords_Detect.sql
@@ -0,0 +1,43 @@
+-- Desc: See if any objects are using reserved keywords
+-- License: ShoutOutWare - give me a shout out on Twitter @bdill if this script helped you. :)  (retain comments and do not redistribute)
+-- Auth: Brian Dill 2020-11-30
+-- Script home: https://github.com/wbdill/SQLServer-sandbox01/adm_ReservedKeywords_Detect
+-- Other useful files: https://github.com/wbdill/SQLServer-sandbox01
+
+
+DROP TABLE #tmpReservedWords
+CREATE TABLE #tmpReservedWords ( val VARCHAR(100) )
+
+INSERT INTO #tmpReservedWords VALUES ('ADD'), ('EXTERNAL'), ('PROCEDURE'), ('ALL'), ('FETCH'), ('PUBLIC'), ('ALTER')
+, ('FILE'), ('RAISERROR'), ('AND'), ('FILLFACTOR'), ('READ'), ('ANY'), ('FOR'), ('READTEXT'), ('AS'), ('FOREIGN')
+, ('RECONFIGURE'), ('ASC'), ('FREETEXT'), ('REFERENCES'), ('AUTHORIZATION'), ('FREETEXTTABLE'), ('REPLICATION')
+, ('BACKUP'), ('FROM'), ('RESTORE'), ('BEGIN'), ('FULL'), ('RESTRICT'), ('BETWEEN'), ('FUNCTION'), ('RETURN')
+, ('BREAK'), ('GOTO'), ('REVERT'), ('BROWSE'), ('GRANT'), ('REVOKE'), ('BULK'), ('GROUP'), ('RIGHT'), ('BY')
+, ('HAVING'), ('ROLLBACK'), ('CASCADE'), ('HOLDLOCK'), ('ROWCOUNT'), ('CASE'), ('IDENTITY'), ('ROWGUIDCOL')
+, ('CHECK'), ('IDENTITY_INSERT'), ('RULE'), ('CHECKPOINT'), ('IDENTITYCOL'), ('SAVE'), ('CLOSE'), ('IF')
+, ('SCHEMA'), ('CLUSTERED'), ('IN'), ('SECURITYAUDIT'), ('COALESCE'), ('INDEX'), ('SELECT'), ('COLLATE')
+, ('INNER'), ('SEMANTICKEYPHRASETABLE'), ('COLUMN'), ('INSERT'), ('SEMANTICSIMILARITYDETAILSTABLE'), ('COMMIT')
+, ('INTERSECT'), ('SEMANTICSIMILARITYTABLE'), ('COMPUTE'), ('INTO'), ('SESSION_USER'), ('CONSTRAINT'), ('IS')
+, ('SET'), ('CONTAINS'), ('JOIN'), ('SETUSER'), ('CONTAINSTABLE'), ('KEY'), ('SHUTDOWN'), ('CONTINUE'), ('KILL')
+, ('SOME'), ('CONVERT'), ('LEFT'), ('STATISTICS'), ('CREATE'), ('LIKE'), ('SYSTEM_USER'), ('CROSS'), ('LINENO')
+, ('TABLE'), ('CURRENT'), ('LOAD'), ('TABLESAMPLE'), ('CURRENT_DATE'), ('MERGE'), ('TEXTSIZE'), ('CURRENT_TIME')
+, ('NATIONAL'), ('THEN'), ('CURRENT_TIMESTAMP'), ('NOCHECK'), ('TO'), ('CURRENT_USER'), ('NONCLUSTERED'), ('TOP')
+, ('CURSOR'), ('NOT'), ('TRAN'), ('DATABASE'), ('NULL'), ('TRANSACTION'), ('DBCC'), ('NULLIF'), ('TRIGGER')
+, ('DEALLOCATE'), ('OF'), ('TRUNCATE'), ('DECLARE'), ('OFF'), ('TRY_CONVERT'), ('DEFAULT'), ('OFFSETS'), ('TSEQUAL')
+, ('DELETE'), ('ON'), ('UNION'), ('DENY'), ('OPEN'), ('UNIQUE'), ('DESC'), ('OPENDATASOURCE'), ('UNPIVOT'), ('DISK')
+, ('OPENQUERY'), ('UPDATE'), ('DISTINCT'), ('OPENROWSET'), ('UPDATETEXT'), ('DISTRIBUTED'), ('OPENXML'), ('USE')
+, ('DOUBLE'), ('OPTION'), ('USER'), ('DROP'), ('OR'), ('VALUES'), ('DUMP'), ('ORDER'), ('VARYING'), ('ELSE'), ('OUTER')
+, ('VIEW'), ('END'), ('OVER'), ('WAITFOR'), ('ERRLVL'), ('PERCENT'), ('WHEN'), ('ESCAPE'), ('PIVOT'), ('WHERE')
+, ('EXCEPT'), ('PLAN'), ('WHILE'), ('EXEC'), ('PRECISION'), ('WITH'), ('EXECUTE'), ('PRIMARY'), ('WITHIN GROUP')
+, ('EXISTS'), ('PRINT'), ('WRITETEXT'), ('EXIT'), ('PROC')
+
+SELECT * FROM sys.tables WHERE [name] IN ( SELECT val FROM #tmpReservedWords )
+
+SELECT * FROM sys.views WHERE [name] IN ( SELECT val FROM #tmpReservedWords )
+
+SELECT * FROM sys.procedures WHERE [name] IN ( SELECT val FROM #tmpReservedWords )
+
+SELECT * FROM sys.objects WHERE type IN ('FN', 'IF', 'TF') AND [name] IN ( SELECT val FROM #tmpReservedWords )
+
+DROP TABLE #tmpReservedWords
+
