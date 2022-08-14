bab3ccc34d05a9811c9ec6f10c497ed2c64f46e2
Brian Dill <brian.dill@randasolutions.com>
2022-08-14 15:35:40 -0500

    minor format changes

--- Last_Exec_time_for_a_stored_procedure.sql
+++ Last_Exec_time_for_a_stored_procedure.sql
@@ -3,26 +3,26 @@
 
 USE MyDatabase
 
-SELECT p.object_id
-     --, d.database_id
-     , DB_NAME(d.database_id)                   AS DatabaseName
-     --, S.name                                   AS SchemaName
-     --, p.name                                   AS SPName
-	 , S.name + '.' + p.name					AS SPName
-     , d.last_execution_time
-	 , DATEDIFF(s, d.last_execution_time, GETDATE()) AS LastExecSecondsAgo
-     , d.cached_time
-     , ISNULL(d.execution_count, 0)             AS ExecCount
-     , d.total_elapsed_time
-     , d.total_elapsed_time / d.execution_count AS avg_elapsed_time
-     , d.last_elapsed_time
-     , p.create_date                            
-     , p.modify_date                            
-FROM sys.procedures                   AS p
-LEFT JOIN sys.dm_exec_procedure_stats AS d ON d.object_id = p.object_id
-LEFT OUTER JOIN sys.schemas           AS S ON S.schema_id = p.schema_id
+SELECT P.object_id
+	--, D.database_id
+	, DB_NAME(d.database_id)					AS DatabaseName
+	--, S.name                                   AS SchemaName
+	--, p.name                                   AS SPName
+	, S.name + '.' + P.name						AS SPName
+	, D.last_execution_time
+	, DATEDIFF(s, D.last_execution_time, GETDATE()) AS LastExecSecondsAgo
+	, D.cached_time
+	, ISNULL(D.execution_count, 0)				AS ExecCount
+	, D.total_elapsed_time
+	, D.total_elapsed_time / d.execution_count	AS avg_elapsed_time
+	, D.last_elapsed_time
+	, P.create_date                            
+	, P.modify_date                            
+FROM sys.procedures                   AS P
+LEFT JOIN sys.dm_exec_procedure_stats AS D ON D.object_id = P.object_id
+LEFT OUTER JOIN sys.schemas           AS S ON S.schema_id = P.schema_id
 
-WHERE p.is_ms_shipped = 0
---AND DATEADD(MINUTE, -60, GETDATE()) < d.last_execution_time -- ran in last 60 minutes
+WHERE P.is_ms_shipped = 0
+--AND DATEADD(MINUTE, -60, GETDATE()) < D.last_execution_time -- ran in last 60 minutes
 
-ORDER BY d.last_execution_time DESC;
+ORDER BY D.last_execution_time DESC;
